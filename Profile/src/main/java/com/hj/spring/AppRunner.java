06330d708593664c7408db0e4f02c0dbf9e0c84a
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-12 15:31:01 +0900

    properites 간단히

--- Profile/src/main/java/com/hj/spring/AppRunner.java
+++ Profile/src/main/java/com/hj/spring/AppRunner.java
@@ -1,8 +1,7 @@
 package com.hj.spring;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -18,10 +17,17 @@ public class AppRunner implements ApplicationRunner {
 	@Autowired
 	BookRepository BookRepository;
 	
+	
+	@Value("${app.name}")
+	String AppName;
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		Environment environment = ctx.getEnvironment();
-		System.out.println(Arrays.toString(environment.getActiveProfiles()));
-		System.out.println(Arrays.toString(environment.getDefaultProfiles()));
-	}
+//		System.out.println(Arrays.toString(environment.getActiveProfiles()));
+//		System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+//		System.out.println(environment.getProperty("app.name"));
+		System.out.println(environment.getProperty("app.about"));
+		System.out.println(AppName);
+	}	
 }
