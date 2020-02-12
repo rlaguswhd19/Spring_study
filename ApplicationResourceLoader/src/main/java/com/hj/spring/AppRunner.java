e94efda61f51c9384d8fcc77ce8b067e7be5ff02
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-12 21:43:18 +0900

    ResourceLoader

---
+++ ApplicationResourceLoader/src/main/java/com/hj/spring/AppRunner.java
@@ -0,0 +1,30 @@
+package com.hj.spring;
+
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner{
+	
+	@Autowired
+	ResourceLoader resourceLoader;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Resource resource = resourceLoader.getResource("classpath:test.txt");
+		System.out.println(resource.exists());
+		System.out.println(resource.getDescription());
+		
+		File file = resource.getFile();
+		
+		System.out.println(Files.readAllLines(file.toPath()));
+	}
+}
---
+++ ApplicationResourceLoader/src/main/java/com/hj/spring/ApplicationResourceLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.hj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationResourceLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationResourceLoaderApplication.class, args);
+	}
+
+}
