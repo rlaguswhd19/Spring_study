9ac8aed5e0f17e60b4a011536a75ee1c604c9a70
rlaguswhd19 <rlaguswhd19@naver.com>
2020-02-13 11:48:22 +0900

    Resource

--- ApplicationResourceLoader/src/main/java/com/hj/spring/AppRunner.java
+++ ApplicationResourceLoader/src/main/java/com/hj/spring/AppRunner.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -15,16 +16,19 @@ import org.springframework.stereotype.Component;
 public class AppRunner implements ApplicationRunner{
 	
 	@Autowired
-	ResourceLoader resourceLoader;
+	ApplicationContext resourceLoader;
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(resourceLoader.getClass());
+		
+		
 		Resource resource = resourceLoader.getResource("classpath:test.txt");
+		System.out.println(resource.getClass());
+		
 		System.out.println(resource.exists());
 		System.out.println(resource.getDescription());
-		
 		File file = resource.getFile();
-		
 		System.out.println(Files.readAllLines(file.toPath()));
 	}
 }
